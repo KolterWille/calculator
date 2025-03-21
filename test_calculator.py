f5be2f0465c5d0bddc49d20be8862d8890bac218
Kolter Wille <kiw0912@cwc.edu>
2025-03-20 20:29:33 -0600

    Intentionally bad

--- test_calculator.py
+++ test_calculator.py
@@ -8,7 +8,7 @@ class TestCalculator(unittest.TestCase):
     
         def test_multiply(self):
             self.assertEqual(multiply(2, 3), 6)
-            self.assertEqual(multiply(1, 0), 0)
+            self.assertEqual(multiply(1, 0), 1)
             self.assertEqual(multiply(0, 0), 0)
             self.assertEqual(multiply(-1, 1), -1)
             self.assertEqual(multiply(-1, -1), 1)
